f362bb09271b2d5eb1bddce0b4e4d1ef8b1e6404
Egor Nazarkin <nimnull@gmail.com>
2012-01-09 00:34:30 +0200

    Updated settings for storing session cookies for a 5 days period

--- app/settings.py
+++ app/settings.py
@@ -6,6 +6,9 @@ DEBUG = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
 
 SECRET_KEY = '<D=f1^F%w8Xr/9?^KR+sYhEOZx8X9jZd3Zdi8)r);F/}\@Stg['
 
+SESSION_COOKIE_NAME = '_gaejst'
+PERMANENT_SESSION_LIFETIME = 432000
+
 SITE_TITLE = 'GAE JS Table'
 
 EMAIL_FROM = 'Gaejs <do-not-reply@gmail.com>'
@@ -15,7 +18,10 @@ ASSETS_URL = "/static/"
 ASSETS_CLOSURE_COMPRESSOR_OPTIMIZATION = 'ADVANCED_OPTIMIZATIONS'
 ASSETS_UPDATER = False
 ASSETS_EXPIRE = False
+
 YUI_COMPRESSOR_PATH = "/home/nimnull/bin/yuicompressor.jar"
+
 CLOSURE_COMPRESSOR_PATH = "/home/nimnull/bin/compiler.jar"
+
 COMPASS_BIN = "/usr/local/bin/compass"
 COMPASS_PLUGINS = ['compass_twitter_bootstrap']
