abaf51440b662f30011aa3feb7b854cbd11f7256
Egor Nazarkin <nimnull@gmail.com>
2012-01-08 21:51:39 +0200

    Updated signin & signout views for proper processing auth and registration

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,17 +1,35 @@
+import logging
+from flask import request, redirect, url_for
+
 from core.decorators import render_to
+
 from . import auth
+from .forms import SignUpForm, SignInForm
+from .models import User
 
 
-@auth.route('/sign_up')
+@auth.route('/sign_up', methods=['GET', 'POST'])
 @render_to()
 def sign_up():
-    return {}
+    form = SignUpForm(len(request.form) and request.form or None)
+    if request.method == 'POST' and form.validate():
+        key = form.save()
+        return redirect(url_for('auth.profile', key=key.urlsafe()))
+    return {'form': form}
 
 
-@auth.route('/sign_in')
+@auth.route('/sign_in', methods=['GET', 'POST'])
 @render_to()
 def sign_in():
-    return {}
+    form = SignInForm(len(request.form) and request.form or None)
+    if request.method == 'POST' and form.validate():
+        user = User.check_password(**form.data)
+        if user:
+            return redirect(url_for('core.index'))
+        else:
+            # have to add flash message creation/rendering
+            pass
+    return {'form': form}
 
 
 @auth.route('/sign_out')
@@ -24,3 +42,10 @@ def sign_out():
 @render_to()
 def recover():
     return {}
+
+
+@auth.route('/profile/<key>')
+@render_to()
+def profile(key):
+    user = User.get_by_urlsafe(key)
+    return {'user': user}
