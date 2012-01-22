995a0c69a2f289f240d8e019cc5b01e2703a2175
Egor Nazarkin <nimnull@gmail.com>
2012-01-22 18:16:18 +0200

    Category listing with pagination and localization support

--- app/catalog/views.py
+++ app/catalog/views.py
@@ -1,9 +1,13 @@
-# import logging
-from flask import request, redirect, url_for
+import logging
+from google.appengine.ext.ndb import context, tasklets
+
+from flask import request, redirect, url_for, render_template
 from flaskext.babel import get_locale
+
 from auth.decorators import login_required
 
 from core.decorators import render_to
+from core.pagination import Pager
 from . import catalog
 from .forms import CategoryForm
 from .models import Category
@@ -22,6 +26,27 @@ def create_category():
 
 @catalog.route('/cats')
 @render_to()
+@login_required
+@context.toplevel
 def list_categories():
-    cats_q = Category.get_localized(get_locale().language)
-    return {'query': cats_q}
+    lang_code = get_locale().language
+    cats_q = Category.get_localized(lang_code)
+    res = yield _list_categories(cats_q, lang_code)
+    raise tasklets.Return(res)
+
+
+@tasklets.tasklet
+def _list_categories(cats_q, lang_code):
+    def localize(cat):
+        for lv in cat.title:
+            if lv.lang == lang_code:
+                setattr(cat, 'l_title', lv.value)
+        return cat
+    pager = Pager(query=cats_q)
+    categories, _, _ = pager.paginate()
+    categories = map(localize, categories)
+    logging.info(categories)
+
+    ctx = {'pager': pager, 'categories': categories}
+
+    raise tasklets.Return(render_template('catalog/list_categories.html', **ctx))
