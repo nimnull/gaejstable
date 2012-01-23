31fa1bc7e1a9133439a01b28d7954cc0db8d75b6
Egor Nazarkin <nimnull@gmail.com>
2012-01-24 01:32:10 +0200

    Added async views for records management.

--- app/catalog/views.py
+++ app/catalog/views.py
@@ -1,11 +1,12 @@
-from flask import request, redirect, url_for
+from flask import g, request, redirect, url_for
+from ndb import context, tasklets
 
 from auth.decorators import login_required
 
 from core.decorators import render_to
 from . import catalog
-from .forms import CategoryForm
-from .models import Category
+from .forms import CategoryForm, RecordForm
+from .models import Category, Record, User2Category
 
 
 @catalog.route('/cats/create', methods=['GET', 'POST'])
@@ -20,8 +21,50 @@ def create_category():
 
 
 @catalog.route('/cats')
-@render_to()
 @login_required
+@render_to()
 def list_categories():
-    cats, pager = Category.paginate_categories()
+    cats, pager = Category.paginate()
     return {'pager': pager, 'categories': cats}
+
+
+@catalog.route('/cats/<key>/records/create', methods=['GET'])
+@catalog.route('/records/create', methods=['POST'])
+@login_required
+@render_to()
+def create_record(key=None):
+    form = RecordForm(len(request.form) and request.form or None,
+            category=key)
+    if request.method == 'POST' and form.validate():
+        form.save()
+        return redirect(url_for('.list_records', key=form.category.data))
+    else:
+        category = Category.get_by_urlsafe(key)
+        return {'form': form, 'category': category}
+
+
+@catalog.route('/cats/<key>/records')
+@login_required
+@render_to()
+@context.toplevel
+def list_records(key):
+    category = yield Category.get_async(key)
+    records, pager = Record.paginate(Record.for_category(key),
+            page_size=10)
+    raise tasklets.Return({
+        'records': records,
+        'pager': pager,
+        'category': category
+    })
+
+
+@catalog.route('/')
+@login_required
+@render_to()
+@context.toplevel
+def filtered_records():
+    categories = User2Category.get_for_user(user=g.user).map_async(
+            lambda u2c: u2c.category)
+    categories = yield categories
+    records, pager = Record.paginate(Record.for_categories(categories))
+    raise tasklets.Return({'records': records, 'pager': pager})
