f73fdf113a5e1f404f2e5225fd6cea5dae8d3a42
Egor Nazarkin <nimnull@gmail.com>
2012-01-28 01:25:20 +0200

    Updated translations and templates paths

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,15 +1,14 @@
 # import logging
 from flaskext.babel import lazy_gettext as _
-from wtforms import (Form, TextField, PasswordField, validators,
-    ValidationError, HiddenField)
-# from wtforms.ext.csrf.session import SessionSecureForm
+from wtforms import (Form, SelectField, TextField, PasswordField,
+        validators, ValidationError, HiddenField)
 
+from app import app
 from core.validators import Email
-
 from .models import User
 
 
-class ProfileForm(Form):
+class UserBaseForm(Form):
     first_name = TextField(_('First Name'), description=_('enter your first '
             'name here'), validators=[validators.Required()])
     last_name = TextField(_('Last Name'), description=_('enter your last '
@@ -19,21 +18,16 @@ class ProfileForm(Form):
 class SignInForm(Form):
     username = TextField(_('Email'), description=_('enter your email '
             'address to sign in'), validators=[Email()])
-    raw_password = PasswordField(_('Password'), description=_('provide your '
+    password = PasswordField(_('Password'), description=_('provide your '
             'password to authenticate yourself'), validators=[
                 validators.Required(message=_("We can't proceed without "
                     "password"))])
 
 
-class SignUpForm(ProfileForm):
-    username = TextField(_('Your Email'), description=_('email will acts '
-        'as your login'), validators=[Email()])
-    password = PasswordField(_('New Password'), description=_('chose a '
-            'password for signing in'), validators=[
-        validators.Required(),
-        validators.EqualTo('confirm', message=_('Passwords must match'))])
+class SignUpForm(UserBaseForm, SignInForm):
     confirm = PasswordField(_('Confirm Password'), description=_('repeat your '
-            'password entered above'))
+            'password entered above'), validators=[
+                validators.EqualTo('password', message=_('Passwords must match'))])
 
     def validate_username(self, field):
         if not User.is_unique(field.data):
@@ -43,6 +37,11 @@ class SignUpForm(ProfileForm):
         user_data = self.data
         del user_data['confirm']
         return User.create(**user_data)
+
+
+class ProfileForm(UserBaseForm):
+    language = SelectField(_('language'), description=_('select '
+        'locale', choice=app.config['LANGUAGES']))
 
 
 class AskRecoverForm(Form):
