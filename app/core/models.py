bb3526d1a436facc238ef5e9c8ee6d0036bef536
Egor Nazarkin <nimnull@gmail.com>
2012-01-30 16:12:02 +0200

    Replaced limited string to the text property with no text limits

--- app/core/models.py
+++ app/core/models.py
@@ -21,7 +21,7 @@ LANG_CHOICES = app.config['LANGUAGES']
 class LangValue(model.Model):
     lang = model.StringProperty(required=True,
             choices=LANG_CHOICES.keys())
-    value = model.StringProperty(required=True,
+    tevalue = model.TextProperty(required=True,
             validator=strip_validator)
 
 
